2aeb777c857c54a264dfbe25309a313fef6764b9
Dusan Cvjeticanin <61889682+cvjdusan@users.noreply.github.com>
2025-01-06 22:33:10 +0100

    Fix gui

--- src/main/scala/MinesweeperApp.scala
+++ src/main/scala/MinesweeperApp.scala
@@ -6,9 +6,8 @@ import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.control.{Alert, ComboBox, ListView, Menu, MenuBar, MenuItem}
 import scalafx.scene.layout.{BorderPane, StackPane, VBox}
 import scalafx.stage.FileChooser
-
 import java.io.{BufferedReader, File, FileReader}
-
+import scalafx.scene.control._
 
 object MinesweeperApp extends JFXApp3 {
 
@@ -31,10 +30,19 @@ object MinesweeperApp extends JFXApp3 {
       disable = true
     }
 
+    val startButton = new Button("Start Game") {
+      disable = true
+    }
+
     difficultyComboBox.onAction = _ => {
       val selectedDifficulty = difficultyComboBox.value.value
       levelsListView.items = ObservableBuffer[String](levels.getOrElse(selectedDifficulty, Seq.empty[String]): _*)
       levelsListView.disable = false
+      startButton.disable = levelsListView.selectionModel().getSelectedItem == null
+    }
+
+    levelsListView.selectionModel().selectedItemProperty().addListener { (_, _, newValue) =>
+      startButton.disable = newValue == null
     }
 
     val controller = new GameController()
@@ -46,7 +54,7 @@ object MinesweeperApp extends JFXApp3 {
           new Menu("Options") {
             items = Seq(
               new MenuItem("New game") {
-                onAction = _ => startNewGame(controller, view, difficultyComboBox, levelsListView, mainLayout)
+                onAction = _ => resetToSelection(difficultyComboBox, levelsListView, startButton, mainLayout)
               },
               new MenuItem("Load game") {
                 onAction = _ => loadGame(controller, view, mainLayout)
@@ -66,10 +74,13 @@ object MinesweeperApp extends JFXApp3 {
         alignment = Pos.Center
         children = Seq(
           difficultyComboBox,
-          levelsListView
+          levelsListView,
+          startButton
         )
       }
     }
+
+    startButton.onAction = _ => startNewGame(controller, view, difficultyComboBox, levelsListView, mainLayout)
 
     stage = new JFXApp3.PrimaryStage {
       title = "Minesweeper"
@@ -84,6 +95,7 @@ object MinesweeperApp extends JFXApp3 {
   private def startNewGame(controller: GameController, view: GameView, difficultyComboBox: ComboBox[String], levelsListView: ListView[String], mainLayout: BorderPane): Unit = {
     val selectedDifficulty = difficultyComboBox.value.value
     val selectedLevel = levelsListView.selectionModel().getSelectedItem
+
     if (selectedDifficulty != null && selectedLevel != null) {
       if (selectedLevel == "Random") {
         println(s"Starting a new game with difficulty: $selectedDifficulty and random level.")
@@ -99,6 +111,22 @@ object MinesweeperApp extends JFXApp3 {
         headerText = "Invalid Selection"
         contentText = "Please select both a difficulty and a level!"
       }.showAndWait()
+    }
+  }
+
+  private def resetToSelection(difficultyComboBox: ComboBox[String], levelsListView: ListView[String], startButton: Button, mainLayout: BorderPane): Unit = {
+    difficultyComboBox.value = null
+    levelsListView.items.value.clear()
+    levelsListView.disable = true
+    startButton.disable = true
+    mainLayout.center = new VBox {
+      spacing = 10
+      alignment = Pos.Center
+      children = Seq(
+        difficultyComboBox,
+        levelsListView,
+        startButton
+      )
     }
   }
 
@@ -132,4 +160,3 @@ object MinesweeperApp extends JFXApp3 {
     println("Show results functionality is not implemented yet.")
   }
 }
-
